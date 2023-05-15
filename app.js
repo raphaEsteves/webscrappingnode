fc68eaa9013f84c7ac38640ae290c2e58ad2ec26
Raphael Martins <raphiusk.rio@gmail.com>
2023-05-15 00:35:45 -0300

    TODO: corrigir buscar por classes no html

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-(async () =>{
-    const navegador = await puppeteer.launch({headless: false});
-    const pagina = await navegador.newPage();
-    pagina.goto('http://google.com.br');
-})(); //auto-executa
+(async () => {
+    const browser = await puppeteer.launch({
+      headless: false
+    });
 
+    const page = await browser.newPage();
+    // await page.goto('https://www.instagram.com/rocketseat/');
+
+    await page.goto('https://unsplash.com/pt-br');
+    await page.setViewport({
+      width: 1080,
+      height: 1024,
+      deviceScaleFactor: 1
+    })
+    await page.type('.ctM_F.gdt34', 'praia');
+    await page.keyboard.press('Enter'); //pressionar tecla enter
+
+    const imgList = await page.evaluate(()=>{
+      // toda essa função será executada no navegador
+
+      // buscar imagens que estão na parte
+      //TODO: corrigir essa busca por classes no HTML
+      const nodeList = document.querySelectorAll('div.search-photos-route img')
+      
+      // transformar o nodeList em array
+      const imgArray = [...nodeList]
+      
+      // transformar os nodes em objetos JS
+      const imgList = imgArray.map( ({src}) => ({
+        src
+      }))
+
+      // colocar para fora da função
+      return imgList;
+    })
+
+    // escrever os dados em um arquivo local (JSON)
+    fs.writeFile('imagens.json', JSON.stringify(imgList, null, 2), err =>{
+      if(err) throw new Error('Algo deu errado')
+      console.log('Ok')
+    });
+})();
